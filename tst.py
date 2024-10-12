7a56e378cfc2abf7da75a677c13b97c0dd0e3050
voronsoft <gbo4.net@gmail.com>
2024-10-12 06:33:51 +0300

    temp commit 4

--- tst.py
+++ tst.py
@@ -1,77 +1,65 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-###########################################################################
-## Python code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO *NOT* EDIT THIS FILE!
-###########################################################################
+import sys
 
 import wx
-import wx.xrc
-
-import gettext
-_ = gettext.gettext
-
-###########################################################################
-## Class MyDialog1
-###########################################################################
-
-class MyDialog1 ( wx.Dialog ):
-
-    def __init__( self, parent ):
-        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.DefaultSize, style = wx.DEFAULT_DIALOG_STYLE )
-
-        self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-
-        bSizer1 = wx.BoxSizer( wx.VERTICAL )
-
-        sizer_lang = wx.BoxSizer( wx.VERTICAL )
-
-        self.m_staticline1 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
-        sizer_lang.Add( self.m_staticline1, 0, wx.EXPAND |wx.ALL, 5 )
-
-        sizer_txt_lang = wx.BoxSizer( wx.VERTICAL )
-
-        self.stat_txt = wx.StaticText( self, wx.ID_ANY, _(u"Выберите язык"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.stat_txt.Wrap( -1 )
-
-        self.stat_txt.SetForegroundColour( wx.Colour( 255, 0, 0 ) )
-
-        sizer_txt_lang.Add( self.stat_txt, 0, 0, 5 )
+import app_locale_base as alb
+import function
 
 
-        sizer_lang.Add( sizer_txt_lang, 0, wx.ALIGN_CENTER, 5 )
+class ErrorWindow(wx.Frame):
+    def __init__(self, parent):
+        super(ErrorWindow, self).__init__(parent, size=(400, 200))
+        self.SetTitle(_("ОШИБКА"))
 
-        sizer_btn_lang = wx.BoxSizer( wx.HORIZONTAL )
+        # Панель и текст
+        panel = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.btn_uk = wx.Button( self, wx.ID_ANY, _(u"Українська"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.btn_uk.SetLabelMarkup( _(u"Українська") )
-        sizer_btn_lang.Add( self.btn_uk, 0, wx.ALL, 5 )
+        # Текст выводится большими буквами
+        self.text = wx.StaticText(panel, label=_("Блокировка интерфейса").upper(), style=wx.ALIGN_CENTER)
+        font = wx.Font(18, wx.DEFAULT, wx.NORMAL, wx.BOLD)
+        self.text.SetFont(font)
+        sizer.Add(self.text, 1, wx.EXPAND | wx.ALL, 20)
 
-        self.btn_en = wx.Button( self, wx.ID_ANY, _(u"English"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        sizer_btn_lang.Add( self.btn_en, 0, wx.ALL, 5 )
+        # Кнопки переключения языков
+        btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        ru_btn = wx.Button(panel, label="Русский")
+        en_btn = wx.Button(panel, label="English")
+        uk_btn = wx.Button(panel, label="Українська")
 
-        self.btn_ru = wx.Button( self, wx.ID_ANY, _(u"Русский"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        sizer_btn_lang.Add( self.btn_ru, 0, wx.ALL, 5 )
+        # Добавляем кнопки в горизонтальный бокс-сайзер
+        btn_sizer.Add(ru_btn, 1, wx.EXPAND | wx.ALL, 5)
+        btn_sizer.Add(en_btn, 1, wx.EXPAND | wx.ALL, 5)
+        btn_sizer.Add(uk_btn, 1, wx.EXPAND | wx.ALL, 5)
+
+        # Добавляем кнопки в основной сайзер
+        sizer.Add(btn_sizer, 0, wx.ALIGN_CENTER)
+        panel.SetSizer(sizer)
+
+        # Обработчики событий для кнопок
+        ru_btn.Bind(wx.EVT_BUTTON, lambda event: self.update_language('ru'))
+        en_btn.Bind(wx.EVT_BUTTON, lambda event: self.update_language('en'))
+        uk_btn.Bind(wx.EVT_BUTTON, lambda event: self.update_language('uk'))
+
+        self.Centre()  # Центрируем окно на экране
+        self.Show()  # Показываем окно
+
+    def update_language(self, lang_code):
+        """
+        Обновление языка и текста
+        """
+        # self.text.SetLabel(_("Блокировка интерфейса").upper())  # Обновляем текст
+        self.Layout()  # Перерисовываем окно
+        function.update_data_json("language", lang_code)  # Сохраняем выбранный язык
 
 
-        sizer_lang.Add( sizer_btn_lang, 0, wx.EXPAND, 5 )
-
-        self.m_staticline2 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
-        sizer_lang.Add( self.m_staticline2, 0, wx.EXPAND |wx.ALL, 5 )
-
-
-        bSizer1.Add( sizer_lang, 1, wx.ALL|wx.EXPAND, 5 )
+def main():
+    app = alb.BaseApp(redirect=False)
+    frame = ErrorWindow(None)
+    frame.Show()
+    app.MainLoop()  # Запускаем главный цикл приложения
 
 
-        self.SetSizer( bSizer1 )
-        self.Layout()
-        bSizer1.Fit( self )
-
-        self.Centre( wx.BOTH )
-
-    def __del__( self ):
-        pass
-
-
+if __name__ == '__main__':
+    main()
